f8165a90d95b1ab4f2463ffbbc097305247af419
WiseRandr <highwiseman@gmail.com>
2019-08-11 08:55:35 +0300

    home page design

--- src/app/utils/sample-usecase/sample-usecase.component.ts
+++ src/app/utils/sample-usecase/sample-usecase.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-sample-usecase',
-  templateUrl: './sample-usecase.component.html',
-  styleUrls: ['./sample-usecase.component.scss']
+  selector: "app-sample-usecase",
+  templateUrl: "./sample-usecase.component.html",
+  styleUrls: ["./sample-usecase.component.scss"]
 })
 export class SampleUsecaseComponent implements OnInit {
+  ObjectKeys = Object.keys;
+  sampleUseCases: string[] = [];
+  selectedUseCaseKey: string = "";
+  selectedUseCaseText: string = "";
 
-  constructor() { }
+  constructor() {
+    this.sampleUseCases["Tweets"] = "Tweets text here";
+    this.sampleUseCases["Online Review"] = "Online Review text here";
+    this.sampleUseCases["Email message"] = "Email message text here";
+    this.sampleUseCases["Product Review in French"] =
+      "Product Review in French text here";
+    this.sampleUseCases["Your own text"] = "Your own text text here";
 
-  ngOnInit() {
+    this.selectUseCase("Tweets");
   }
 
+  ngOnInit() {}
+
+  selectUseCase(useCasekey: string) {
+    this.selectedUseCaseKey = useCasekey;
+    this.selectedUseCaseText = this.sampleUseCases[useCasekey];
+  }
 }
